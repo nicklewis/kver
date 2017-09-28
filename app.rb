708cc32b3c8f6fed842586da1ce5ee226c566eb2
Nick Lewis <nick@puppetlabs.com>
2017-09-28 15:36:28 -0700

    Add rack config

--- app.rb
+++ app.rb
@@ -1,28 +1,30 @@
 require 'json'
-require 'sinatra'
+require 'sinatra/base'
 require 'pp'
 
-set :kvs, {}
+class KVApp < Sinatra::Base
+  set :kvs, {}
 
-get '/kv/:key' do
-  if settings.kvs.include?(params[:key])
-    [200, settings.kvs[params[:key]].to_json]
-  else
-    [404, "Key not found"]
+  get '/kv/:key' do
+    if settings.kvs.include?(params[:key])
+      [200, settings.kvs[params[:key]].to_json]
+    else
+      [404, "Key not found"]
+    end
   end
-end
 
-post '/kv' do
-  vals = JSON.parse(request.body.read)
-  vals.each do |k,v|
-    settings.kvs[k] = v
+  post '/kv' do
+    vals = JSON.parse(request.body.read)
+    vals.each do |k,v|
+      settings.kvs[k] = v
+    end
+    204
   end
-  204
-end
 
-put '/kv/:key' do
-  value = JSON.parse(request.body.read)
-  settings.kvs[params[:key]] = value
-  puts Thread.current
-  204
+  put '/kv/:key' do
+    value = JSON.parse(request.body.read)
+    settings.kvs[params[:key]] = value
+    puts Thread.current
+    204
+  end
 end
